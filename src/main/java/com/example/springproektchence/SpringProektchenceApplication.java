5fc64b6087236b864a6938a1975d4da1786bc224
SDDimitrov18 <SDDimitrov18@codingburgas.bg>
2023-01-23 12:47:43 +0200

    Initial commitnnkmmmm

---
+++ src/main/java/com/example/springproektchence/SpringProektchenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springproektchence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProektchenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProektchenceApplication.class, args);
+    }
+
+}
